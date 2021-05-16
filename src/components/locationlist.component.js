5a0caabd7567545413c5ddf744657964e50f432d
pomelooooooo <suwijak1691@gmail.com>
2021-05-16 21:29:39 +0700

    add alert box

--- src/components/locationlist.component.js
+++ src/components/locationlist.component.js
@@ -8,33 +8,38 @@ const Exercise = (props) => (
       <img class="img-fluid" src={props.exercise.image} alt="" />
     </div>
     <div class="col-lg-6">
-          <div class="project-text w-100 my-auto text-center text-lg-left">
-          <h3 class="text-black">{props.exercise.cetagory}</h3>
-            <h4 class="text-black">{props.exercise.name}</h4>
-            <h6 class="mb-0 text-black-60">The place: {props.exercise.place}</h6>
-            <h6 class="mb-0 text-black-60">Organizer: {props.exercise.organizer}</h6>
-            <h6 class="mb-0 text-black-60">Date: {props.exercise.date.substring(0, 10)}</h6>
-            <h6 class="mb-0 text-black-60">Detail: {props.exercise.detail}</h6>
-            <hr class="d-none d-lg-block mb-0 ml-0" />
-            <br />
-            <Link
-              class="btn btn-primary js-scroll-trigger"
-              to={"/edit/" + props.exercise._id}
-            >
-              edit
-            </Link>{" "}
-            |{" "}
-            <a
-              class="btn btn-primary js-scroll-trigger"
-              href="#"
-              onClick={() => {
-                props.deleteExercise(props.exercise._id);
-              }}
-            >
-              delete
-            </a>
-          </div>
-        </div>  
+      <div class="project-text w-100 my-auto text-center text-lg-left">
+        <h3 class="text-black">{props.exercise.cetagory}</h3>
+        <h4 class="text-black">{props.exercise.name}</h4>
+        <h6 class="mb-0 text-black-60">The place: {props.exercise.place}</h6>
+        <h6 class="mb-0 text-black-60">
+          Organizer: {props.exercise.organizer}
+        </h6>
+        <h6 class="mb-0 text-black-60">
+          Date: {props.exercise.date.substring(0, 10)}
+        </h6>
+        <h6 class="mb-0 text-black-60">Detail: {props.exercise.detail}</h6>
+        <hr class="d-none d-lg-block mb-0 ml-0" />
+        <br />
+        <Link
+          class="btn btn-primary js-scroll-trigger"
+          to={"/edit/" + props.exercise._id}
+        >
+          edit
+        </Link>{" "}
+        |{" "}
+        <a
+          class="btn btn-primary js-scroll-trigger"
+          href="#"
+          onClick={() => {
+            props.deleteExercise(props.exercise._id);
+            alert("Are you sure to delete data!");
+          }}
+        >
+          delete
+        </a>
+      </div>
+    </div>
   </div>
 );
 
